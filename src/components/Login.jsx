5aca1c76e954a52ca69895c6ed491631539dd937
ujwal <ujwalmogha2004@gmail.com>
2024-05-04 22:50:37 +0530

    improved ui of contact form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -51,7 +51,7 @@ const SignUpPage = () => {
                     newErrors[err.path] = err.message;
                 }
             });
-           setErrors(newErrors);
+            setErrors(newErrors);
         }
     }
 
@@ -78,76 +78,200 @@ const SignUpPage = () => {
         })
     }
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            <div>
-                <div className="formField" >
-                    <label >First Name :</label>
-                    <input type="text" name="firstName" value={formData.firstName} placeholder="Enter firstName" onChange={handleChange} />
-                    {errors.firstName && <div className="error">{errors.firstName}</div>}
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <form className="flex justify-center m-5 p-3" onSubmit={handleSubmit}>
+                <div className="max-w-md w-full">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+                            First Name:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="firstName"
+                            type="text"
+                            name="firstName"
+                            value={formData.firstName}
+                            placeholder="Enter First Name"
+                            onChange={handleChange}
+                        />
+                        {errors.firstName && <div className="text-red-500 text-xs italic">{errors.firstName}</div>}
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+                            Last Name:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="lastName"
+                            type="text"
+                            name="lastName"
+                            value={formData.lastName}
+                            placeholder="Enter Last Name"
+                            onChange={handleChange}
+                        />
+                        {errors.lastName && <div className="text-red-500 text-xs italic">{errors.lastName}</div>}
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                            Email:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email"
+                            type="text"
+                            name="email"
+                            value={formData.email}
+                            placeholder="Enter Email"
+                            onChange={handleChange}
+                        />
+                        {errors.email && <div className="text-red-500 text-xs italic">{errors.email}</div>}
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phoneNumber">
+                            Phone Number:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="phoneNumber"
+                            type="text"
+                            name="phoneNumber"
+                            value={formData.phoneNumber}
+                            placeholder="Enter Phone Number"
+                            autoComplete="tel"
+                            onChange={handleChange}
+                        />
+                        {errors.phoneNumber && <div className="text-red-500 text-xs italic">{errors.phoneNumber}</div>}
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            placeholder="Enter Password"
+                            autoComplete="new-password"
+                            onChange={handleChange}
+                        />
+                        {errors.password && <div className="text-red-500 text-xs italic">{errors.password}</div>}
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+                            Confirm Password:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="confirmPassword"
+                            type="password"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            placeholder="Confirm Password"
+                            autoComplete="new-password"
+                            onChange={handleChange}
+                        />
+                        {errors.confirmPassword && <div className="text-red-500 text-xs italic">{errors.confirmPassword}</div>}
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="age">
+                            Age:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="age"
+                            type="number"
+                            name="age"
+                            value={formData.age}
+                            placeholder="Enter Your Age"
+                            onChange={handleChange}
+                        />
+                        {errors.age && <div className="text-red-500 text-xs italic">{errors.age}</div>}
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="gender">
+                            Gender:
+                        </label>
+                        <select
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="gender"
+                            name="gender"
+                            value={formData.gender}
+                            onChange={handleChange}
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                        {errors.gender && <div className="text-red-500 text-xs italic">{errors.gender}</div>}
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2">Interests:</label>
+                        <div className="flex flex-wrap">
+                            <label className="mr-4">
+                                <input
+                                    type="checkbox"
+                                    name="coding"
+                                    checked={formData.interests.includes("coding")}
+                                    onChange={handleCheckboxChange}
+                                    className="mr-2 leading-tight"
+                                />
+                                Coding
+                            </label>
+                            <label className="mr-4">
+                                <input
+                                    type="checkbox"
+                                    name="sports"
+                                    checked={formData.interests.includes("sports")}
+                                    onChange={handleCheckboxChange}
+                                    className="mr-2 leading-tight"
+                                />
+                                Sports
+                            </label>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    name="reading"
+                                    checked={formData.interests.includes("reading")}
+                                    onChange={handleCheckboxChange}
+                                    className="mr-2 leading-tight"
+                                />
+                                Reading
+                            </label>
+                        </div>
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="birthDate">
+                            Date of Birth:
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="birthDate"
+                            type="date"
+                            name="birthDate"
+                            value={formData.birthDate}
+                            onChange={handleChange}
+                        />
+                        {errors.birthDate && <div className="text-red-500 text-xs italic">{errors.birthDate}</div>}
+                    </div>
+
+                    <div className="mb-6">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+
                 </div>
-                <div className="formField">
-                    <label>Last Name :</label>
-                    <input type="text" name="lastName" value={formData.lastName} placeholder="Enter lastName" onChange={handleChange} />
-                    {errors.lastName && <div className="error">{errors.lastName}</div>}
-                </div>
-                <div className="formField">
-                    <label>Email :</label>
-                    <input type="text" name="email" value={formData.email} placeholder="Enter EmailName" onChange={handleChange} />
-                    {errors.email && <div className="error">{errors.email}</div>}
-                </div>
-                <div className="formField">
-                    <label>Phone Number :</label>
-                    <input type="text" name="phoneNumber" value={formData.phoneNumber} placeholder="Enter phoneNumber"  autoComplete="tel" onChange={handleChange} />
-                    {errors.phoneNumber && <div className="error">{errors.phoneNumber}</div>}
-                </div>
-                <div className="formField">
-                    <label>Password :</label>
-                    <input type="password" name="password" value={formData.password} placeholder="Enter password" autoComplete="new-password"  onChange={handleChange} />
-                    {errors.password && <div className="error">{errors.password}</div>}
-                </div>
-                <div className="formField">
-                    <label>Confirm Password:</label>
-                    <input type="password" name="confirmPassword" value={formData.confirmPassword} placeholder="Enter password" autoComplete="new-password" onChange={handleChange} />
-                    {errors.confirmPassword && <div className="error">{errors.confirmPassword}</div>}
-                </div>
-                <div className="formField">
-                    <label>Age :</label>
-                    <input type="number" name="age" value={formData.age} placeholder="Enter your age" onChange={handleChange} />
-                    {errors.age && <div className="error">{errors.age}</div>}
-                </div>
-                <div className="formField"> <label>Gender :</label>
-                    <select name="gender" value={formData.gender} onChange={handleChange}>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">other</option>
-                    </select>
-                    {errors.gender && <div className="error">{errors.gender}</div>}
-                </div>
-                <div className="formField"> <label>Interests :</label>
-                    <label>
-                        <input type="checkbox" name="coding" checked={formData.interests.includes("coding")} onChange={handleCheckboxChange}/>
-                        Coding
-                    </label>
-                    <label>
-                        <input type="checkbox" name="sports" checked={formData.interests.includes("sports")} onChange={handleCheckboxChange} />
-                        Sports
-                    </label>
-                    <label>
-                        <input type="checkbox" name="reading" checked={formData.interests.includes("reading")} onChange={handleCheckboxChange} />
-                        Reading
-                    </label>
-                </div>
-                <div className="formField"> <label>Date of Birth :</label>
-                    <input type="date" name="birthDate" value={formData.birthDate} placeholder="Enter your DOB" onChange={handleChange} />
-                </div>
-                {errors.birthDate && <div className="error">{errors.birthDate}</div>}
-               <div className="formField">
-                <button   className="submitButton" type="Submit">Submit</button>
-              </div>
-            </div>
-        </form>
+            </form >
+        </div>
 
     )
 }
 
 export default SignUpPage;
+
+
